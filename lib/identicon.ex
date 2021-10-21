83b31412607bf507f62be3e2bdea10bed7a6dceb
William Vincent <william@william-vincent.dev>
2021-10-21 02:41:07 -0400

    finished

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,12 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def hash_input(input) do
@@ -10,5 +16,60 @@ defmodule Identicon do
       |> :binary.bin_to_list()
 
     %Identicon.Image{seed: seed_input}
+  end
+
+  def pick_color(%Identicon.Image{seed: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{seed: seed} = image) do
+    grid =
+      seed
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter(grid, fn {num, _idx} ->
+        rem(num, 2) == 0
+      end)
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_num, idx}) ->
+      horizontal = rem(idx, 5) * 50
+      vertical = div(idx, 5) * 50
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+      {top_left, bottom_right}
+    end
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, file_name) do
+    File.write("#{file_name}.png", image)
   end
 end
